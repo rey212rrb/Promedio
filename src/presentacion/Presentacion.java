fafb6a91dbc71d01608c62995bfafc6fac005d60
Eduardo21-rrb <rosales.basurto.eduardorey.ipn@gmail.com>
2026-01-10 18:58:11 -0600

    Clase Presentacion

---
+++ src/presentacion/Presentacion.java
@@ -0,0 +1,35 @@
+package presentacion;
+
+import logica.Logica;
+
+import java.util.Scanner;
+
+public class Presentacion {
+
+        private static Scanner sc = new Scanner(System.in);
+
+        public static void main(String[] args){
+
+            System.out.println("Promedio ");
+
+            System.out.println("Cuantos numeros ingresara: ");
+            int tamanio = sc.nextInt();
+
+            int[] arreglo = new int[tamanio];
+
+            System.out.println("Ingrese " + tamanio + " numeros: ");
+
+            for(int i = 0; i < arreglo.length; i++){
+
+                System.out.print("Numero " + i + ": ");
+                arreglo[i] = sc.nextInt();
+
+            }
+
+            var promedio = Logica.media(arreglo);
+
+            System.out.println("El primedio es: " + promedio);
+
+        }
+
+}
